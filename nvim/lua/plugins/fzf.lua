352774c427347b7bc002fef927f9b79edb8619da
Yashver1 <yashvershori@gmail.com>
2026-05-27 11:20:24 +0800

    plugin: Add more fzf keys and update nix

--- nvim/lua/plugins/fzf.lua
+++ nvim/lua/plugins/fzf.lua
@@ -5,16 +5,101 @@ return {
 	config = function(_, opts)
 		local fzf = require("fzf-lua")
 		fzf.setup({ "max-perf" })
-		vim.keymap.set("n", "<leader>/", "<cmd>FzfLua grep<CR>", {
-			desc = "grep text from cwd",
-		})
-
-		vim.keymap.set("n", "<leader>f", "<cmd>FzfLua files<CR>", {
-			desc = "find files from cwd",
-		})
-
-		vim.keymap.set("n", "<leader>s", "<cmd>FzfLua treesitter<CR>", {
-			desc = "find symbols from current window",
-		})
 	end,
+	keys = {
+		{ "<c-j>", "<c-j>", ft = "fzf", mode = "t", nowait = true },
+		{ "<c-k>", "<c-k>", ft = "fzf", mode = "t", nowait = true },
+		{
+			"<leader>,",
+			"<cmd>FzfLua buffers sort_mru=true sort_lastused=true<cr>",
+			desc = "Switch Buffer",
+		},
+		{ "<leader>/", "<cmd>FzfLua live_grep<cr>", desc = "Grep" },
+		{ "<leader>:", "<cmd>FzfLua command_history<cr>", desc = "Command History" },
+		{ "<leader>fb", "<cmd>FzfLua buffers sort_mru=true sort_lastused=true<cr>", desc = "Buffers" },
+		{ "<leader>fB", "<cmd>FzfLua buffers<cr>", desc = "Buffers (all)" },
+		{
+			"<leader>fc",
+			function()
+				require("fzf-lua").files({ cwd = vim.fn.stdpath("config") })
+			end,
+			desc = "Find Config File",
+		},
+		{ "<leader>ff", "<cmd>FzfLua files<cr>", desc = "Find Files" },
+		{ "<leader>fg", "<cmd>FzfLua git_files<cr>", desc = "Find Files (git-files)" },
+		{ "<leader>fr", "<cmd>FzfLua oldfiles<cr>", desc = "Recent" },
+		{
+			"<leader>fR",
+			function()
+				require("fzf-lua").oldfiles({ cwd = vim.uv.cwd() })
+			end,
+			desc = "Recent (cwd)",
+		},
+		{ "<leader>gc", "<cmd>FzfLua git_commits<CR>", desc = "Commits" },
+		{ "<leader>gd", "<cmd>FzfLua git_diff<cr>", desc = "Git Diff (files)" },
+		{ "<leader>gl", "<cmd>FzfLua git_commits<CR>", desc = "Commits" },
+		{ "<leader>gs", "<cmd>FzfLua git_status<CR>", desc = "Status" },
+		{ "<leader>gS", "<cmd>FzfLua git_stash<cr>", desc = "Git Stash" },
+		{ '<leader>s"', "<cmd>FzfLua registers<cr>", desc = "Registers" },
+		{ "<leader>s/", "<cmd>FzfLua search_history<cr>", desc = "Search History" },
+		{ "<leader>sa", "<cmd>FzfLua autocmds<cr>", desc = "Auto Commands" },
+		{ "<leader>sb", "<cmd>FzfLua lines<cr>", desc = "Buffer Lines" },
+		{ "<leader>sc", "<cmd>FzfLua command_history<cr>", desc = "Command History" },
+		{ "<leader>sC", "<cmd>FzfLua commands<cr>", desc = "Commands" },
+		{ "<leader>sd", "<cmd>FzfLua diagnostics_workspace<cr>", desc = "Diagnostics" },
+		{ "<leader>sD", "<cmd>FzfLua diagnostics_document<cr>", desc = "Buffer Diagnostics" },
+		{ "<leader>sg", "<cmd>FzfLua live_grep<cr>", desc = "Grep" },
+		{
+			"<leader>sG",
+			function()
+				require("fzf-lua").live_grep({ cwd = vim.uv.cwd() })
+			end,
+			desc = "Grep (cwd)",
+		},
+		{ "<leader>sh", "<cmd>FzfLua help_tags<cr>", desc = "Help Pages" },
+		{ "<leader>sH", "<cmd>FzfLua highlights<cr>", desc = "Search Highlight Groups" },
+		{ "<leader>sj", "<cmd>FzfLua jumps<cr>", desc = "Jumplist" },
+		{ "<leader>sk", "<cmd>FzfLua keymaps<cr>", desc = "Key Maps" },
+		{ "<leader>sl", "<cmd>FzfLua loclist<cr>", desc = "Location List" },
+		{ "<leader>sM", "<cmd>FzfLua man_pages<cr>", desc = "Man Pages" },
+		{ "<leader>sm", "<cmd>FzfLua marks<cr>", desc = "Jump to Mark" },
+		{ "<leader>sR", "<cmd>FzfLua resume<cr>", desc = "Resume" },
+		{ "<leader>sq", "<cmd>FzfLua quickfix<cr>", desc = "Quickfix List" },
+		{ "<leader>sw", "<cmd>FzfLua grep_cword<cr>", desc = "Word" },
+		{
+			"<leader>sW",
+			function()
+				require("fzf-lua").grep_cword({ cwd = vim.uv.cwd() })
+			end,
+			desc = "Word (cwd)",
+		},
+		{ "<leader>sw", "<cmd>FzfLua grep_visual<cr>", mode = "x", desc = "Selection" },
+		{
+			"<leader>sW",
+			function()
+				require("fzf-lua").grep_visual({ cwd = vim.uv.cwd() })
+			end,
+			mode = "x",
+			desc = "Selection (cwd)",
+		},
+		{ "<leader>uC", "<cmd>FzfLua colorschemes<cr>", desc = "Colorscheme with Preview" },
+		{
+			"<leader>ss",
+			function()
+				require("fzf-lua").lsp_document_symbols({
+					regex_filter = symbols_filter,
+				})
+			end,
+			desc = "Goto Symbol",
+		},
+		{
+			"<leader>sS",
+			function()
+				require("fzf-lua").lsp_live_workspace_symbols({
+					regex_filter = symbols_filter,
+				})
+			end,
+			desc = "Goto Symbol (Workspace)",
+		},
+	},
 }
