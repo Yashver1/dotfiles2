2b3e5ec1746625abc51afbb41420c98f6c98fd3d
Yashver1 <yashvershori@gmail.com>
2026-05-27 23:03:16 +0800

    plugin: Add nvim tree

--- nvim/lua/plugins/filesystem.lua
+++ nvim/lua/plugins/filesystem.lua
@@ -1,48 +1,12 @@
 return {
 	{
-		"mikavilpas/yazi.nvim",
-		version = "*",
-		event = "VeryLazy",
-		dependencies = {
-			{ "nvim-lua/plenary.nvim", lazy = true },
-		},
-		keys = {
-			{
-				"<leader>-",
-				mode = { "n", "v" },
-				"<cmd>Yazi<cr>",
-				desc = "Open yazi at the current file",
-			},
-			{
-				"<leader>cw",
-				"<cmd>Yazi cwd<cr>",
-				desc = "Open the file manager in nvim's working directory",
-			},
-			{
-				"<c-up>",
-				"<cmd>Yazi toggle<cr>",
-				desc = "Resume the last yazi session",
-			},
-		},
-		opts = {
-			yazi_floating_window_border = "rounded",
-			open_for_directories = true,
-			keymaps = {
-				show_help = "<f1>",
-			},
-		},
-		init = function()
-			vim.g.loaded_netrwPlugin = 1
-		end,
-	},
-	{
 		"stevearc/oil.nvim",
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 		keys = {
 			{ "-", "<cmd>Oil<cr>", desc = "Open Parent Directory" },
 		},
 		opts = {
-			default_file_explorer = true, -- Replaces netrw automatically
+			default_file_explorer = true,
 			view_options = {
 				show_hidden = true,
 			},
@@ -56,6 +20,51 @@ return {
 
 				["<C-x>"] = "actions.select_split",
 				["<C-v>"] = "actions.select_vsplit",
+			},
+		},
+	},
+
+	{
+		"benomahony/oil-git.nvim",
+		dependencies = {
+			"stevearc/oil.nvim",
+		},
+	},
+	{
+		"nvim-tree/nvim-tree.lua",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		keys = {
+			{ "<leader>e", "<cmd>NvimTreeToggle<CR>", desc = "Toggle File Tree" },
+		},
+		opts = {
+			disable_netrw = true,
+			hijack_netrw = true,
+
+			view = {
+				width = 30,
+				side = "right",
+				preserve_window_proportions = true,
+			},
+			renderer = {
+				group_empty = true,
+				highlight_git = true,
+				icons = {
+					show = {
+						git = true,
+						folder = true,
+						file = true,
+						folder_arrow = false,
+					},
+				},
+			},
+			actions = {
+				open_file = {
+					quit_on_open = false,
+				},
+			},
+			filters = {
+				dotfiles = false,
+				custom = { "^.git$" },
 			},
 		},
 	},
