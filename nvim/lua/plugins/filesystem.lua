424faa8272430d989201697d60ae82e051c20521
Yashver1 <yashvershori@gmail.com>
2026-06-12 18:54:17 +0800

    nvim: update filesystem and profile.lua

--- nvim/lua/plugins/filesystem.lua
+++ nvim/lua/plugins/filesystem.lua
@@ -1,71 +1,74 @@
 return {
-	{
-		"stevearc/oil.nvim",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		keys = {
-			{ "-", "<cmd>Oil<cr>", desc = "Open Parent Directory" },
-		},
-		opts = {
-			default_file_explorer = true,
-			view_options = {
-				show_hidden = true,
-			},
-			keymaps = {
-				["<C-s>"] = false,
+  {
+    "stevearc/oil.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    keys = {
+      { "-", "<cmd>Oil<cr>", desc = "Open Parent Directory" },
+    },
+    opts = {
+      default_file_explorer = true,
+      view_options = {
+        show_hidden = true,
+      },
+      keymaps = {
+        ["<C-s>"] = false,
 
-				["<C-h>"] = "<cmd>TmuxNavigateLeft<CR>",
-				["<C-j>"] = "<cmd>TmuxNavigateDown<CR>",
-				["<C-k>"] = "<cmd>TmuxNavigateUp<CR>",
-				["<C-l>"] = "<cmd>TmuxNavigateRight<CR>",
+        ["<C-h>"] = "<cmd>TmuxNavigateLeft<CR>",
+        ["<C-j>"] = "<cmd>TmuxNavigateDown<CR>",
+        ["<C-k>"] = "<cmd>TmuxNavigateUp<CR>",
+        ["<C-l>"] = "<cmd>TmuxNavigateRight<CR>",
 
-				["<C-x>"] = "actions.select_split",
-				["<C-v>"] = "actions.select_vsplit",
-			},
-		},
-	},
+        ["<C-x>"] = "actions.select_split",
+        ["<C-v>"] = "actions.select_vsplit",
+      },
+    },
+  },
 
-	{
-		"benomahony/oil-git.nvim",
-		dependencies = {
-			"stevearc/oil.nvim",
-		},
-	},
-	{
-		"nvim-tree/nvim-tree.lua",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		keys = {
-			{ "<leader>e", "<cmd>NvimTreeToggle<CR>", desc = "Toggle File Tree" },
-		},
-		opts = {
-			disable_netrw = true,
-			hijack_netrw = true,
+  {
+    "benomahony/oil-git.nvim",
+    dependencies = {
+      "stevearc/oil.nvim",
+    },
+  },
+  {
+    "nvim-tree/nvim-tree.lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    keys = {
+      { "<leader>e", "<cmd>NvimTreeToggle<CR>", desc = "Toggle File Tree" },
+    },
+    opts = {
+      disable_netrw = true,
+      hijack_netrw = true,
+      update_focused_file = {
+        enable = true
+      },
 
-			view = {
-				width = 30,
-				side = "right",
-				preserve_window_proportions = true,
-			},
-			renderer = {
-				group_empty = true,
-				highlight_git = true,
-				icons = {
-					show = {
-						git = true,
-						folder = true,
-						file = true,
-						folder_arrow = false,
-					},
-				},
-			},
-			actions = {
-				open_file = {
-					quit_on_open = false,
-				},
-			},
-			filters = {
-				dotfiles = false,
-				custom = { "^.git$" },
-			},
-		},
-	},
+      view = {
+        width = 30,
+        side = "right",
+        preserve_window_proportions = true,
+      },
+      renderer = {
+        group_empty = true,
+        highlight_git = true,
+        icons = {
+          show = {
+            git = true,
+            folder = true,
+            file = true,
+            folder_arrow = false,
+          },
+        },
+      },
+      actions = {
+        open_file = {
+          quit_on_open = false,
+        },
+      },
+      filters = {
+        dotfiles = false,
+        custom = { "^.git$" },
+      },
+    },
+  },
 }
--- nvim/lua/plugins/profile.lua
+++ nvim/lua/plugins/profile.lua
@@ -1,16 +1,17 @@
 return {
-	"folke/snacks.nvim",
-	opts = function()
-		Snacks.toggle.profiler():map("<leader>pp")
-		Snacks.toggle.profiler_highlights():map("<leader>ph")
-	end,
-	keys = {
-		{
-			"<leader>ps",
-			function()
-				Snacks.profiler.scratch()
-			end,
-			desc = "Profiler Scratch Bufer",
-		},
-	},
+  "folke/snacks.nvim",
+  enabled = true,
+  opts = function()
+    Snacks.toggle.profiler():map("<leader>pp")
+    Snacks.toggle.profiler_highlights():map("<leader>ph")
+  end,
+  keys = {
+    {
+      "<leader>ps",
+      function()
+        Snacks.profiler.scratch()
+      end,
+      desc = "Profiler Scratch Bufer",
+    },
+  },
 }
