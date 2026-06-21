603233fd0f2fb8160d7e4f00f679789da5054a5d
Yashver1 <yashvershori@gmail.com>
2026-06-21 21:53:23 +0800

    nvim: disable snacks

--- nvim/lua/plugins/profile.lua
+++ nvim/lua/plugins/profile.lua
@@ -1,6 +1,6 @@
 return {
   "folke/snacks.nvim",
-  enabled = true,
+  enabled = false,
   opts = function()
     Snacks.toggle.profiler():map("<leader>pp")
     Snacks.toggle.profiler_highlights():map("<leader>ph")
